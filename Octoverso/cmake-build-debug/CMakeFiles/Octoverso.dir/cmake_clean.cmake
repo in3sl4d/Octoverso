02577c5f0dd08dee1f301897be3f9d85b037178c
in3sl4d <ines.ladjenef@etu.u-paris.fr>
2024-12-29 21:19:20 +0100

    MAJ 29.12.2024

---
+++ Octoverso/cmake-build-debug/CMakeFiles/Octoverso.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Octoverso.dir/dictionnaire.c.obj"
+  "CMakeFiles/Octoverso.dir/dictionnaire.c.obj.d"
+  "CMakeFiles/Octoverso.dir/jeu.c.obj"
+  "CMakeFiles/Octoverso.dir/jeu.c.obj.d"
+  "CMakeFiles/Octoverso.dir/joueur.c.obj"
+  "CMakeFiles/Octoverso.dir/joueur.c.obj.d"
+  "CMakeFiles/Octoverso.dir/main.c.obj"
+  "CMakeFiles/Octoverso.dir/main.c.obj.d"
+  "Octoverso.exe"
+  "Octoverso.exe.manifest"
+  "Octoverso.pdb"
+  "libOctoverso.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Octoverso.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
